44cf1aa0f178d7a504bad737728770351419d300
rajib hossen <mhossen6311@gmail.com>
2025-07-02 02:00:34 +0600

    booking set update

--- client/src/Components/DateSelect.jsx
+++ client/src/Components/DateSelect.jsx
@@ -14,7 +14,7 @@ const DateSelect = ({ dateTime, id }) => {
         if (!selected) {
             return toast('Please select a date')
          }
-        navigate('/movies/${id}/${selected}')
+        navigate(`/movies/${id}/${selected}`)
         scrollTo(0, 0)
     }
 
--- client/src/Components/Loading.jsx
+++ client/src/Components/Loading.jsx
@@ -3,7 +3,7 @@
 const Loading = () => {
     return (
         <div className='flex justify-center items-center h-[80vh]'>
-            <div className='animate-spin rounded-full h-14 w-14 border-2 border-t-primary'></div>l
+            <div className='animate-spin rounded-full h-14 w-14 border-2 border-t-primary'></div>
         </div>
     );
 };
