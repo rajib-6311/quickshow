c3a504047c74490b8c91d556e738eaad248cb58b
rajib hossen <mhossen6311@gmail.com>
2025-07-01 17:37:15 +0600

    update You May Also Like

--- client/src/Pages/MovieDetails.jsx
+++ client/src/Pages/MovieDetails.jsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import BlurCircle from "../Components/BlurCircle";
 import { Heart, PlayCircleIcon, StarIcon } from "lucide-react";
 import timeFormat from "../Lib/timeFormat";
+import DateSelect from "../Components/DateSelect";
+import MoveCard from "../Components/MoveCard";
+import Loading from "../Components/Loading";
 
 const MovieDetails = () => {
+    const navigate = useNavigate()
     const { id } = useParams();
     const [show, setShow] = useState(null);
 
@@ -20,10 +24,10 @@ const MovieDetails = () => {
     }, [id]);
 
     if (!show) {
-        return <div>Loading...</div>;
+        return <Loading/>
     }
 
-    return (
+    return(
         <div className="px-6 md:px-16 lg:px-40 pt-30 md:pt-50">
             <div className="flex flex-col md:flex-row gap-8 max-w-6xl mx-auto">
                 <img
@@ -52,18 +56,50 @@ const MovieDetails = () => {
 
                     <div className="flex items-center flex-wrap gap-4 mt-4">
                         <button className="flex items-center gap-2 px-7 py-3 text-sm bg-gray-800 hover:bg-gray-900 transition rounded-md font-medium cursor-pointer active:scale-95">
-                            <PlayCircleIcon className="w-5 h-5"/>
+                            <PlayCircleIcon className="w-5 h-5" />
                             Watch Tailer
                         </button>
-                        <a className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95" href="">Buy Tickets</a>
+
+                        <a className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95" href="#dateSelect">Buy Tickets</a>
+
                         <button className="bg-gary-700 p-2.5 rounded-full transition cursor-pointer active:scale-95">
                             <Heart className={`w-5 h-5`} />
                         </button>
                     </div>
                 </div>
             </div>
+
+
+            <p className="text-lg font-medium mt-20">Your Favorite Cast</p>
+            <div className="overflow-x-auto no-scrollbar mt-8 pb-4">
+                <div className="flex items-center gap-4 w-max px-4">
+                    {
+                        show.movie.casts.slice(0, 10).map((cast, index) => (
+                            <div key={index} className="flex flex-col items-center text-center">
+                                <img src={cast.profile_path} alt="" className="rounded-full h-20 md:h-20 aspect-square object-cover" />
+                                <p className="text-xs font-medium mt-3">{cast.name}</p>
+                            </div>
+                        ))
+                    }
+                </div>
+
+            </div>
+            <DateSelect dateTime={show.dateTime} id={id} />
+
+            <p className="text-lg font-medium mt-20 mb-8">You May Also Like</p>
+            <div className="flex flex-wrap max-sm:justify-center gap-8">
+                {
+                    dummyShowsData.slice(0, 4).map((movie, index) => (
+                        <MoveCard key={index} movie={movie} />
+                    ))
+                }
+            </div>
+
+            <div className='flex justify-center mt-20'>
+                <button onClick={()=>{navigate('/movies'); scrollTo(0,0)}} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Show more</button>
+            </div>
         </div>
-    );
+    ) 
 };
 
 export default MovieDetails;
