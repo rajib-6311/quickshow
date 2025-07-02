2e5d87236d69d8ee844c36bd3f837541ddcb7c9f
rajib hossen <mhossen6311@gmail.com>
2025-07-02 20:24:12 +0600

    update sets page

--- client/src/Pages/SeatLayout.jsx
+++ client/src/Pages/SeatLayout.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { dummyDateTimeData, dummyShowsData } from "../assets/assets";
+import { assets, dummyDateTimeData, dummyShowsData } from "../assets/assets";
 import Loading from "../Components/Loading";
 import { ClockIcon } from "lucide-react";
+import isoTimeFormat from "../Lib/isoTimeFormat";
+import BlurCircle from "../Components/BlurCircle";
+import toast from "react-hot-toast";
 
 
 const SeatLayout = () => {
+
+    const groupRows = [["A", "B"], ["C", "D"], ["E", "F"], ["G", "H"], ["I", "J"]]
 
     const { id, date } = useParams()
     const [selectedSeats, setSelectedSeats] = useState([])
@@ -24,6 +29,36 @@ const SeatLayout = () => {
         }
     }
 
+    const handleSeatClick = (seatId) => {
+        if (!selectedTime) {
+            return toast("Please select time first")
+        }
+        if (!selectedSeats.includes(seatId) && selectedSeats.length > 4) {
+            return toast("You can only select 5 seats")
+        }
+        setSelectedSeats(prev => prev.includes(seatId) ? prev.filter(seat => seat !==
+            seatId) : [...prev, seatId])
+    }
+
+
+    const renderSeats = (row, count = 9) => (
+        <div key={row} className="flex gap-2 mt-2">
+            <div className="flex flex-wrap items-center justify-center gap-2">
+                {Array.from({ length: count }, (_, i) => {
+                    const seatId = `${row}${i + 1}`;
+                    return (
+                        <button key={seatId} onClick={() => handleSeatClick(seatId)} className="{`h-8 w-8 rounded border border-primary/60 cursor-pointer ${selectedSeats.includes(seatId) && 'bg-primary text-white'}`}">
+                            {seatId}
+                        </button>
+                    )
+                })}
+
+            </div>
+
+        </div>
+    )
+
+
     useEffect(() => {
         getShow()
     }, [])
@@ -36,16 +71,33 @@ const SeatLayout = () => {
                 <p className="text-lg font-semibold px-6">Available Timing</p>
                 <div className="mt-5 space-y-1">
                     {show.dateTime[date].map((item) => (
-                        <div className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/20"}`}>
+                        <div key={item.time} onClick={() => setSelectedTime(item)}
+
+                            className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === item.time ? "bg-primary text-white" : "hover:bg-primary/20"}`}>
                             <ClockIcon className="w-4 h-4" />
-                            <p className='text-sm'>{item.time}</p>
+                            <p className='text-sm'>{isoTimeFormat(item.time)}</p>
                         </div>
                     ))}
                 </div>
 
             </div>
             {/* seats layout  */}
-            <div></div>
+            <div className='relative flex-1 flex flex-col items-center max-md:mt-16'>
+                <BlurCircle top="-100px" left="-100px" />
+                <BlurCircle bottom="0" right="0" />
+                <h1 className='text-2x1 font-semibold mb-4'>Select your seat</h1>
+                <img src={assets.screenImage} alt="screen" />
+                <p className='text-gray-400 text-sm mb-6'>SCREEN SIDE</p>
+
+                <div className='flex flex-col items-center mt-10 text-xs text-gray-300'>
+                    <div className='grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6'>
+                        {groupRows[0].map(row => renderSeats(row))}
+                    </div>
+                </div>
+
+                
+
+            </div>
         </div>
     ) : (<Loading />)
 };
